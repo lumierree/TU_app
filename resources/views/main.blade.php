dc978176560974e27ae081313dee166cb6d1d2be
lumierree <arthaparulian31@gmail.com>
2024-10-21 10:49:54 +0700

    Update main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -47,10 +47,10 @@
         <ul class="sidebar">
           <li onclick=hideSidebar()><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
           <li><a href="{{url('/')}}">Dashboard</a></li>
-            <li><a href="{{url('klapper')}}">Klapper</a></li>
-        <li><a href="{{url('/')}}">Coming Soon</a></li>
-        <li><a href="{{url('/')}}">Coming Soon</a></li>
-        <li><a href="{{url('/')}}">Coming Soon</a></li>
+          <li><a href="{{url('klapper')}}">Klapper</a></li>
+          <li><a href="{{url('/')}}">Coming Soon</a></li>
+          <li><a href="{{url('/')}}">Coming Soon</a></li>
+          <li><a href="{{url('/')}}">Coming Soon</a></li>
         </ul>
         <ul>
           <li><h1>Tata Usaha</h1></li>
